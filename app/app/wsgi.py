a584a1e3380a944f104d89b07392065421e13f6c
MichealOmojola <75433192+OmoMicheal@users.noreply.github.com>
2022-08-26 22:13:03 +0100

    asdf

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-
+from django.contrib.auth import get_user_model
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
+
+# added this
+User = get_user_model()
+users = User.objects.all()
+if not users:
+    User.objects.create_superuser(email="admin@example.com", password="admin")
